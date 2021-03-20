6eaecf3d604541f311292c7b2eee2a621cea4cfd
Neberbatov <dk-misis@yandex.ru>
2021-03-20 22:15:40 +0300

    full lesson 5.2

--- MyThirdApplication210221/app/src/main/java/com/example/mythirdapplication210221/Classes.kt
+++ MyThirdApplication210221/app/src/main/java/com/example/mythirdapplication210221/Classes.kt
@@ -7,18 +7,25 @@ fun main() {
             wheelCount = 4, doorCount = 2, bodyLength = 3150.0,     //задаем параметры через конструктор
             bodyWidth = 2150.0, bodyHeight = 1750.0
     )
-    println("tesla wheelCount = ${tesla.wheelCount}. Current speed = ${tesla.currentSpeed}")
-    tesla.currentSpeed = 150
-    println("Current speed = ${tesla.currentSpeed}")
+//    println("tesla wheelCount = ${tesla.wheelCount}. Current speed = ${tesla.currentSpeed}")
+//    tesla.currentSpeed = 150
+//    println("Current speed = ${tesla.currentSpeed}")
 
     val nissan = Car (doorCount = 5)        // изменяем значение по умолчанию
     println("${nissan.bodyHeight}  ${nissan.doorCount}")
 
 
-    //создаем объет через дополнительный конструктор
-    val opel = Car(4,2, Triple(5000.0, 2500.0, 1800.0))
+//    //создаем объет через дополнительный конструктор
+//    val opel = Car(4,2, Triple(5000.0, 2500.0, 1800.0))
+//
+//    println("Высота Опеля: ${opel.bodyHeight} мм")
 
-    println("Высота Опеля: ${opel.bodyHeight} мм")
+    tesla.accelerate(10)           //вызываем метод accelerate класса Car
+    tesla.decelerate(200)
+
+//    tesla.currentSpeed = 100
+//    println(tesla.currentSpeed)
+//    println(tesla.fuelCount)
 
 }
 
--- MyThirdApplication210221/app/src/main/java/com/example/mythirdapplication210221/oop/Car.kt
+++ MyThirdApplication210221/app/src/main/java/com/example/mythirdapplication210221/oop/Car.kt
@@ -7,12 +7,52 @@ class Car constructor(
         val bodyWidth: Double = 2500.0,
         val bodyHeight: Double = 1500.0
 ){
-    init {
-        println("Car init")
-    }
-    var currentSpeed:Int = 0
+//    init {
+//        println("Car init")
+//    }
+     var currentSpeed:Int = 0
+        get() {
+            println("не надо обращаться к currentSpeed")
+            return field        // нельзя return currentSpeed - ибо рекурсия. Работаем через field
+        }
+        private set(value) {
+            println("не надо изменять currentSpeed")
+            field = value
+        }
 
-    //создаем дополнительный конструктор для класса
-    constructor(wheelCount: Int, doorCount: Int, bodySize: Triple<Double, Double, Double>):  // тип Triple содержит три значения
-            this (wheelCount, doorCount, bodySize.first, bodySize.second, bodySize.third)
+    //можно указать private для сеттера, т.о. защитив от изменений (сама переменная доступна для чтения)
+    var fuelCount:Int = 50
+        private set
+
+    // Мы сделали currentSpeed - private, поэтому введем булеан, который будет показывать если 0.
+//    val isStopped: Boolean
+//        get() = currentSpeed==0
+
+//    //создаем дополнительный конструктор для класса
+//    constructor(wheelCount: Int, doorCount: Int, bodySize: Triple<Double, Double, Double>):  // тип Triple содержит три значения
+//            this (wheelCount, doorCount, bodySize.first, bodySize.second, bodySize.third)
+
+    fun accelerate (speed: Int) {           //добавляем метод в класс
+        val needFuel = speed/2
+
+        if (fuelCount >=needFuel) {
+            currentSpeed += speed
+            useFuel(needFuel)               // добавлем метод внутрь другого метода - расход топлива при ускорении
+        } else {
+            println("Не хватает топлива для ускорения")
+        }
+    }
+
+
+    fun decelerate (speed: Int) {
+        currentSpeed= maxOf (0, currentSpeed-speed)
+    //функция maxOf возвращает одно из двух чисел (то, которое больше).
+    // Таким образом не позволим скорости быть меньше 0
+    }
+    private fun useFuel (fuel: Int) {               //делаем метод доступным только в классе
+        fuelCount-=fuel
+        this.fuelCount
+    }
+
+
 }
