92d691ed6dbb7f6568ef16da4cab5d8f4daab722
Neberbatov <dk-misis@yandex.ru>
2021-06-07 23:08:00 +0300

    App June

---
+++ TestAppJune/app/src/main/java/com/example/testappjune/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.testappjune
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.textView1)
+        textView.text = """
+            BuildType = ${BuildConfig.BUILD_TYPE}
+            Flavor = ${BuildConfig.FLAVOR}
+            Version Name = ${BuildConfig.VERSION_NAME}
+        """
+    }
+
+}
